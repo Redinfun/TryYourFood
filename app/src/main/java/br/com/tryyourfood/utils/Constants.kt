68833f030451d76e75b1028b349d8c70fdb584ee
ravelinejunior <junior.raveline@gmail.com>
2021-03-05 10:31:55 -0300

    Created the DataStore Repository

--- app/src/main/java/br/com/tryyourfood/utils/Constants.kt
+++ app/src/main/java/br/com/tryyourfood/utils/Constants.kt
@@ -2,11 +2,12 @@ package br.com.tryyourfood.utils
 
 class Constants {
 
-    companion object{
+    companion object {
         //apontamento para API
         const val BASE_URL = "https://api.spoonacular.com"
+
         //chave de api
-       const val API_KEY = "45334d825076402ba6ff877e8ef65cd6"
+        const val API_KEY = "45334d825076402ba6ff877e8ef65cd6"
 
         //Api query keys
         const val QUERY_NUMBER = "number"
@@ -19,5 +20,15 @@ class Constants {
         //Room
         const val RECIPES_TABLE_NAME = "recipes_table"
         const val RECIPES_DATABASE_NAME = "recipes_database"
+
+        //Bottom Sheet and Preferences
+        const val PREFERENCES_NAME = "foody_preferences"
+        const val DEFAULT_MEAL_TYPE = "main_course"
+        const val DEFAULT_DIET_TYPE = "gluten free"
+        const val DEFAULT_RECIPES_NUMBER = "50"
+        const val PREFERENCES_MEAL_TYPE = "mealType"
+        const val PREFERENCES_MEAL_TYPE_ID = "mealTypeId"
+        const val PREFERENCES_DIET_TYPE = "dietType"
+        const val PREFERENCES_DIET_TYPE_ID = "dietTypeId"
     }
 }
--- app/src/main/java/br/com/tryyourfood/viewmodel/RecipesViewModel.kt
+++ app/src/main/java/br/com/tryyourfood/viewmodel/RecipesViewModel.kt
@@ -3,6 +3,9 @@ package br.com.tryyourfood.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import br.com.tryyourfood.utils.Constants
+import br.com.tryyourfood.utils.Constants.Companion.DEFAULT_DIET_TYPE
+import br.com.tryyourfood.utils.Constants.Companion.DEFAULT_MEAL_TYPE
+import br.com.tryyourfood.utils.Constants.Companion.DEFAULT_RECIPES_NUMBER
 import br.com.tryyourfood.utils.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import br.com.tryyourfood.utils.Constants.Companion.QUERY_API_KEY
 import br.com.tryyourfood.utils.Constants.Companion.QUERY_DIET
@@ -14,11 +17,10 @@ class RecipesViewModel(application: Application) : AndroidViewModel(application)
 
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
-        queries[QUERY_NUMBER] = "100"
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = Constants.API_KEY
-        queries[QUERY_TYPE] = "main course"
-        //queries[QUERY_TYPE] = "snack"
-        queries[QUERY_DIET] = "gluten free"
+        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
