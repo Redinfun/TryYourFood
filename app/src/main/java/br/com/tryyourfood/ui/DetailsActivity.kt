d77173a4151b61a07619a77f57f34a91c0913bff
ravelinejunior <junior.raveline@gmail.com>
2021-03-10 21:58:17 -0300

    Design Details Layout

---
+++ app/src/main/java/br/com/tryyourfood/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package br.com.tryyourfood.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.tryyourfood.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
