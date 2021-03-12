8bcd1bf5a5a93c0988d473c7cea465203ba4c6e2
ravelinejunior <junior.raveline@gmail.com>
2021-03-12 16:15:56 -0300

    Display Ingredients data on fragment

--- app/src/main/java/br/com/tryyourfood/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/br/com/tryyourfood/fragments/instructions/InstructionsFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import br.com.tryyourfood.R
+import br.com.tryyourfood.model.Result
+import br.com.tryyourfood.utils.Constants.Companion.RECIPE_BUNDLE_KEY
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -15,7 +19,16 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(RECIPE_BUNDLE_KEY)
+
+        view.instructionsWebView_id.webViewClient = object : WebViewClient(){}
+        val webSiteUrl:String = myBundle!!.sourceUrl
+        view.instructionsWebView_id.loadUrl(webSiteUrl)
+
+        return view
     }
 
 
