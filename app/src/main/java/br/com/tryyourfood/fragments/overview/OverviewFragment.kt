20172403d4ca2a498adeb185b48bf829d967920d
ravelinejunior <junior.raveline@gmail.com>
2021-03-12 00:35:01 -0300

    Fix the big of not call right type of meal

--- app/src/main/java/br/com/tryyourfood/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/br/com/tryyourfood/fragments/overview/OverviewFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import br.com.tryyourfood.R
+import br.com.tryyourfood.model.Result
+import coil.load
+import kotlinx.android.synthetic.main.fragment_overview.view.*
 
 class OverviewFragment : Fragment() {
 
@@ -14,7 +18,111 @@ class OverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        val view = inflater.inflate(R.layout.fragment_overview, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable("recipeBundle")
+
+        view.imageRecipe_overview_id.load(myBundle?.image)
+        view.title_textView_overview_id.text = myBundle?.title
+        view.qtdLike_overview_id.text = myBundle?.aggregateLikes.toString()
+        view.qtdTime_overview_id.text = myBundle?.readyInMinutes.toString()
+        view.summary_overview_id.text = myBundle?.summary
+
+        if (myBundle?.vegetarian == true) {
+            view.vegeterian_imageView_overview_id.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.vegeterian_textView_overview_id.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+
+        if (myBundle?.vegan == true) {
+            view.vegan_imageView_overview_id.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.vegan_textView_overview_id.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.glutenFree == true) {
+            view.glutten_imageView_overview_id.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.glutten_textView_overview_id.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.veryHealthy == true) {
+            view.healthy_imageView_overview_id.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.healthy_textView_overview_id.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.dairyFree == true) {
+            view.dairy_imageView_overview_id.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.dairy_textView_overview_id.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.cheap == true) {
+            view.cheap_imageView_overview_id.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            view.cheap_textView_overview_id.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+
+
+        return view
     }
 
 }
