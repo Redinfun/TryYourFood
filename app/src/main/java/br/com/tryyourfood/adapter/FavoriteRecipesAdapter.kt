62f9f7a1d966833707dd7fbfbdbce64ee5bb745d
ravelinejunior <junior.raveline@gmail.com>
2021-03-16 15:32:03 -0300

    Adapter Favorite Recipes created and binded

--- app/src/main/java/br/com/tryyourfood/adapter/FavoriteRecipesAdapter.kt
+++ app/src/main/java/br/com/tryyourfood/adapter/FavoriteRecipesAdapter.kt
@@ -2,14 +2,13 @@ package br.com.tryyourfood.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import br.com.tryyourfood.data.database.entities.FavoriteEntity
 import br.com.tryyourfood.databinding.ItemFavoriteRecipesRowBinding
 import br.com.tryyourfood.utils.RecipesDiffUtil
 
-class FavoriteRecipesAdapter: RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
+class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
     private var favoritesList = emptyList<FavoriteEntity>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -25,23 +24,24 @@ class FavoriteRecipesAdapter: RecyclerView.Adapter<FavoriteRecipesAdapter.MyView
         return favoritesList.size
     }
 
-    fun setData(newFavoriteRecipes:List<FavoriteEntity>){
-        val favoriteDiffUtil = RecipesDiffUtil(favoritesList,newFavoriteRecipes)
+    fun setData(newFavoriteRecipes: List<FavoriteEntity>) {
+        val favoriteDiffUtil = RecipesDiffUtil(favoritesList, newFavoriteRecipes)
         val diffUtilResult = DiffUtil.calculateDiff(favoriteDiffUtil)
         favoritesList = newFavoriteRecipes
         diffUtilResult.dispatchUpdatesTo(this)
     }
 
-    class MyViewHolder(private val binding:ItemFavoriteRecipesRowBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(favoriteEntity: FavoriteEntity){
+    class MyViewHolder(private val binding: ItemFavoriteRecipesRowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(favoriteEntity: FavoriteEntity) {
             binding.favoritesEntity = favoriteEntity
             binding.executePendingBindings()
         }
 
         companion object {
-            fun from(parent: ViewGroup):MyViewHolder{
+            fun from(parent: ViewGroup): MyViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = ItemFavoriteRecipesRowBinding.inflate(layoutInflater,parent,false)
+                val binding = ItemFavoriteRecipesRowBinding.inflate(layoutInflater, parent, false)
                 return MyViewHolder(binding)
             }
         }
