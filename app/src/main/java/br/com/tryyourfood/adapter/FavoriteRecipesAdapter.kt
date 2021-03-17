de569947e3ede1817cc463769e7d19d9e1376d08
ravelinejunior <junior.raveline@gmail.com>
2021-03-16 23:43:25 -0300

    OnLongClick Event with contextual action

--- app/src/main/java/br/com/tryyourfood/adapter/FavoriteRecipesAdapter.kt
+++ app/src/main/java/br/com/tryyourfood/adapter/FavoriteRecipesAdapter.kt
@@ -1,17 +1,20 @@
 package br.com.tryyourfood.adapter
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.*
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import br.com.tryyourfood.R
 import br.com.tryyourfood.data.database.entities.FavoriteEntity
 import br.com.tryyourfood.databinding.ItemFavoriteRecipesRowBinding
 import br.com.tryyourfood.fragments.favorite.FavoriteRecipesFragmentDirections
 import br.com.tryyourfood.utils.RecipesDiffUtil
 import kotlinx.android.synthetic.main.item_favorite_recipes_row.view.*
 
-class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
+class FavoriteRecipesAdapter(private val requireActivity: FragmentActivity) :
+    RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>(),
+    ActionMode.Callback {
     private var favoritesList = emptyList<FavoriteEntity>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -31,6 +34,13 @@ class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyVie
             holder.itemView.findNavController().navigate(action)
 
         }
+
+        /* OnLongClickListener */
+        holder.itemView.item_favoritesRow_layout.setOnLongClickListener {
+            requireActivity.startActionMode(this)
+            true
+        }
+
     }
 
     override fun getItemCount(): Int {
@@ -58,5 +68,22 @@ class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyVie
                 return MyViewHolder(binding)
             }
         }
+    }
+
+    override fun onCreateActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
+        actionMode?.menuInflater?.inflate(R.menu.menu_favorite_longclick, menu)
+        return true
+    }
+
+    override fun onPrepareActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
+        return true
+    }
+
+    override fun onActionItemClicked(actionMode: ActionMode?, menu: MenuItem?): Boolean {
+        return true
+    }
+
+    override fun onDestroyActionMode(actionMode: ActionMode?) {
+        return true
     }
 }
